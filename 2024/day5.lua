9c802e8ce7769ba295f1434cb38959e474f4b21b
mierenhoop <46939409+mierenhoop@users.noreply.github.com>
2024-12-05 07:37:24 +0100

    2024 day 5 part 1

---
+++ 2024/day5.lua
@@ -0,0 +1,36 @@
+local input = io.open"day5.txt":read"*a"
+
+local first, second = input:match"^(.-)\n\n(.+)$"
+
+local l = {}
+local r = {}
+for a, b in first:gmatch"(%d+)|(%d+)" do
+  a = tonumber(a)
+  b = tonumber(b)
+  r[b] = r[b] or {}
+  r[b][a] = true
+  l[a] = l[a] or {}
+  l[a][b] = true
+end
+
+local sum = 0
+for line in second:gmatch"[^\n]+" do
+  local ok = true
+  local tt = {}
+  for v in line:gmatch"%d+" do
+    v = tonumber(v)
+    table.insert(tt, v)
+  end
+  local ok = true
+  for i = #tt, 1, -1 do
+    for j = i-1,1,-1 do
+      if not r[tt[i]] or not r[tt[i]][tt[j]] then
+        ok = false
+      end
+    end
+  end
+  if ok then
+    sum = sum + (tt[#tt//2+1])
+  end
+end
+print(sum)
