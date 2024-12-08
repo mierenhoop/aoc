4c852e1cfcf27d9d1ee5a575220c6e7777452444
mierenhoop <46939409+mierenhoop@users.noreply.github.com>
2024-12-08 06:24:41 +0100

    2024 day 8 part 2

--- 2024/day8.lua
+++ 2024/day8.lua
@@ -18,12 +18,11 @@ end
 
 local w, h = #t[1], #t
 local antis={}
-function add(x,y)
+local function add(x,y)
   if 1<=x and x<=w and 1<=y and y <=h then
     antis[x..","..y] = true
   end
 end
-
 for f, tt in pairs(freqs) do
   for i = 1, #tt do
     for j = i+1, #tt do
@@ -34,11 +33,45 @@ for f, tt in pairs(freqs) do
     end
   end
 end
-
---print(require"inspect"(freqs))
-
 local sum=0
 for f in pairs(antis) do
   sum=sum+1
 end
 print(sum)
+
+antis={}
+for f, tt in pairs(freqs) do
+  for i = 1, #tt do
+    for j = i+1, #tt do
+      local dx = tt[i][1]-tt[j][1]
+      local dy = tt[i][2]-tt[j][2]
+      local xx = tt[i][1]
+      local yy = tt[i][2]
+      while 1<=xx and xx<=w and 1<=yy and yy<=h do
+        xx = xx-dx
+        yy = yy-dy
+        add(xx,yy)
+      end
+      xx = tt[j][1]
+      yy = tt[j][2]
+      while 1<=xx and xx<=w and 1<=yy and yy<=h do
+        xx = xx+dx
+        yy = yy+dy
+        add(xx,yy)
+      end
+    end
+  end
+end
+sum=0
+for f in pairs(antis) do
+  sum=sum+1
+end
+print(sum)
+
+
+--for yy=1,h do
+--  for xx=1,w do
+--    io.write(antis[xx..","..yy] and "#" or ".")
+--  end
+--  print""
+--end
