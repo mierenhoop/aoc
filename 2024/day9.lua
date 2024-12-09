5cb817871e41e50d61a6ed3ab2d1175fd2ccdda4
mierenhoop <46939409+mierenhoop@users.noreply.github.com>
2024-12-09 10:30:30 +0100

    2024 day 9 part 2

--- 2024/day9.lua
+++ 2024/day9.lua
@@ -1,12 +1,14 @@
 local line = io.open"day9.txt":read"*l"
 local x = 1
 local t = {}
+local tt={}
 local id=0
 for n, free in line:gmatch"(%d)(%d?)" do
   n=tonumber(n)
   free=tonumber(free) or 0
   for i = 1,n do
     t[x]=id
+    tt[x]=id
     x=x+1
   end
   for i = 1,free do
@@ -14,6 +16,7 @@ for n, free in line:gmatch"(%d)(%d?)" do
   end
   id=id+1
 end
+local mx=x
 for i = 1, x do
   if not t[i] then
     while x > i and not t[x] do
@@ -25,5 +28,37 @@ end
 local sum = 0
 for i = 1, #t do
   sum=sum+(i-1)*t[i]
+end
+print(sum)
+x=mx
+t=tt
+while x>1 do
+  if t[x] then
+    local v = t[x]
+    local i = 0
+    while t[x] == v do
+      i=i+1
+      x=x-1
+    end
+    local free=0
+    for b=1,x do
+      if t[b] then
+        free=0
+      else
+        free=free+1
+        if free==i then
+          for l=1,i do
+            t[b-i+l],t[x+l]=t[x+l],nil
+          end
+        end
+      end
+    end
+  else
+    x=x-1
+  end
+end
+sum = 0
+for i = 1, mx do
+  sum=sum+(i-1)*(t[i] or 0)
 end
 print(sum)
